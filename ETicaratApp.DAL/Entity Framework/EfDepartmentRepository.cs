2c011fc8db4240a0534999fac56b21c19ee44e93
ebruramogluu <118280321+ebruramogluu@users.noreply.github.com>
2023-05-27 15:49:00 +0300

    emloyee k

---
+++ ETicaratApp.DAL/Entity Framework/EfDepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaratApp.Entities;
+using ETicaretApp.DAL.Abstarct;
+using ETicaretApp.DAL.Repostories;
+
+namespace ETicaretApp.DAL.Entity_Framework
+{
+    public class EfDepartmentRepository : GenericRepository<Department> , IDepartmentDal
+    {
+    }
+}
