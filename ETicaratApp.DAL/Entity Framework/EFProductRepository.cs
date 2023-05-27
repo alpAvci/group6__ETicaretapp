069f2b030ebbfa8606c82850e56ed902c7f1bd10
Alp Avcı <alp_avicii@hotmail.com>
2023-05-27 15:50:11 +0300

    DAL/EF 'de değişiklik

---
+++ ETicaratApp.DAL/Entity Framework/EFProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ETicaratApp.Entities;
+using ETicaretApp.DAL.Abstarct;
+using ETicaretApp.DAL.Repostories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.DAL.Entity_Framework
+{
+    public class EFProductRepository : GenericRepository<Product>, IProductDal
+    {
+    }
+}
