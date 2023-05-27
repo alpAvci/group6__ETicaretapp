172f11f8bd32a13c4cffaf5d71d0a7f3ad18e965
Alp Avcı <alp_avicii@hotmail.com>
2023-05-27 21:04:49 +0300

    değişiklik

--- ETicaratApp/Controllers/Category.cs
+++ ETicaratApp/Controllers/Category.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ETicaretApp.BLL.Concrete;
+using ETicaretApp.DAL.Entity_Framework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaretApp.Panel.UI.Controllers
 {
     public class Category : Controller
     {
+
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+
+            var values = cm.GetList();
+
+            return View(values);
         }
     }
 }
