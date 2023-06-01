0f5655b168cce8b08a6299158b35f0001cac97d4
Alp Avcı <alp_avicii@hotmail.com>
2023-06-01 13:36:46 +0300

    son güncelleme

--- ETicaratApp/Program.cs
+++ ETicaratApp/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=access}/{id?}");
+    pattern: "{controller=Category}/{action=index}/{id?}");
 
 app.Run();
